2a0e3612950b46b351d7c565260eefa9e354c6ac
emirob <emiroberti@icloud.com>
2022-03-13 12:07:10 +0000

    added views

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require("express")
 
 const app = express()
+app.set('view engine', 'html');
 
 var port = process.env.port || 3000
 
 app.get("/", (req, res) => {
-    res.render("index.html")
+    res.render("index")
 })
 
 app.listen(port, (req, res) => {
