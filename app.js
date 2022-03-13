8d20491700536a1172ef5e2b4e3f6797d4f7d63e
emirob <emiroberti@icloud.com>
2022-03-13 12:28:25 +0000

    added the Path module

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express")
+const path = require("path")
 
 const app = express()
 app.set('view engine', 'html');
@@ -6,9 +7,11 @@ app.set('view engine', 'html');
 var port = process.env.port || 3000
 
 app.get("/", (req, res) => {
-    res.send("homepage by Emi")
+    //res.send("homepage by Emi")
+    res.sendFile(path.join(__dirname + "/views/index.html"))
 })
 
 app.listen(port, (req, res) => {
     console.log("listening on " + port)
+    console.log(__dirname)
 })
