c94b9c0af573af07867039a21a7e563a2fce94c7
emirob <emiroberti@icloud.com>
2022-03-14 10:26:15 +0000

    y

--- app.js
+++ app.js
@@ -2,16 +2,20 @@ const express = require("express")
 const path = require("path")
 
 const app = express()
-const router = express.Router()
+//const router = express.Router()
 
 app.set('view engine', 'html');
 
 var port = process.env.port || 3000
-app.use("/views", router)
+//app.use("/views", router)
 
-router.get("/views", (req, res) => {
+app.get("/", (req, res) => {
     //res.send("homepage by Emi")
     res.sendFile(path.join(__dirname + "/views/index.html"))
+})
+
+app.get('/smaller', (req, res) => {
+    res.sendFile(path.join(__dirname + "/views/smaller.html"))
 })
 
 app.listen(port, (req, res) => {
